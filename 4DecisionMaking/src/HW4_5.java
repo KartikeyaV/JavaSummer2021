e92fea5d7711ede3f25a3184afcf83c403223911
ZEPHYRUS\karti <kartikeya.vinaykumar@gmail.com>
2021-06-29 12:29:13 -0400

    HW4_5

---
+++ 4DecisionMaking/src/HW4_5.java
@@ -0,0 +1,54 @@
+/**
+ * HW4_5
+ * @author karti
+ *
+ */
+public class HW4_5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num = RUI.readUserIntInput("Enter the Month Number:");
+		
+		switch (num) {
+		case 1: 
+			System.out.println("Jan");
+			break;
+		case 2: 
+			System.out.println("Feb");
+			break;
+		case 3: 
+			System.out.println("Mar");
+			break;
+		case 4: 
+			System.out.println("Apr");
+			break;
+		case 5: 
+			System.out.println("May");
+			break;
+		case 6: 
+			System.out.println("Jun");
+			break;
+		case 7: 
+			System.out.println("Jul");
+			break;
+		case 8: 
+			System.out.println("Aug");
+			break;
+		case 9: 
+			System.out.println("Sep");
+			break;
+		case 10: 
+			System.out.println("Oct");
+			break;
+		case 11: 
+			System.out.println("Nov");
+			break;
+		case 12: 
+			System.out.println("Dec");
+			break;
+		default: 
+			System.out.println("That's not a month");
+		}
+	}
+
+}
