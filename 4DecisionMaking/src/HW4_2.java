f1aac63aeed82b68310a4396d13f32f9f84ac6ee
ZEPHYRUS\karti <kartikeya.vinaykumar@gmail.com>
2021-06-28 22:57:59 -0400

    HW4_2

--- 4DecisionMaking/src/HW4_2.java
+++ 4DecisionMaking/src/HW4_2.java
@@ -6,9 +6,12 @@
 public class HW4_2 {
 
 	public static void main(String[] args) {
-		int loan = RUI.readUserIntInput("Enter Your Loan Amount:");
-		int interestRate = RUI.readUserIntInput("Enter Your Interest Rate:");
-		int period = RUI.readUserIntInput("Enter the Loan Period in Months: ");
+		double loan = RUI.readUserIntInput("Enter Your Loan Amount:");
+		double interestRate = RUI.readUserIntInput("Enter Your Interest Rate:");
+		double rate = interestRate / (12*100);
+		double period = RUI.readUserIntInput("Enter the Loan Period in Months: ");
+		double EMI = (loan*rate*Math.pow(1+rate,period))/(Math.pow(1+rate,period)-1);
+		System.out.println(EMI);
 	}
 
 }
