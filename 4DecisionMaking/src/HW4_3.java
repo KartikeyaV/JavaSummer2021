8b5ffff6311981f2d6e773e0a0040ac93cd57825
ZEPHYRUS\karti <kartikeya.vinaykumar@gmail.com>
2021-06-28 23:09:25 -0400

    HW4_3

--- 4DecisionMaking/src/HW4_3.java
+++ 4DecisionMaking/src/HW4_3.java
@@ -7,7 +7,9 @@ public class HW4_3 {
 
 	public static void main(String[] args) {
 		float grade = RUI.readUserIntInput("Enter Your Grade:");
-		if (grade > 90) {
+		if (grade > 100) {
+			System.out.println("Stop Cheating Beta");
+		} else if (grade > 90) {
 			System.out.println("You have an A+!");
 		} else if (grade > 80){
 			System.out.println("You have an A!");
@@ -17,8 +19,10 @@ public class HW4_3 {
 			System.out.println("You have an B!");
 		} else if (grade > 50){
 			System.out.println("You have an C!");
-		} else {
+		} else if (grade >= 0){
 			System.out.println("You Failed");
+		} else {
+			System.out.println("Your Grade Can't Be That Low Beta");
 		}
 
 	}
