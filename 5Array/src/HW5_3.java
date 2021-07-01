635adefe3808eea5e0d1ffd2220b09c0014e33c4
ZEPHYRUS\karti <kartikeya.vinaykumar@gmail.com>
2021-06-30 21:30:59 -0400

    HW5_3

--- 5Array/src/HW5_3.java
+++ 5Array/src/HW5_3.java
@@ -12,7 +12,19 @@ public class HW5_3 {
 		System.out.println("FC 		Fahrenheit -> Celsius");
 		System.out.println("CF		Celsius -> Fahrenheit");
 		String key = RUI.readUserStringInput("Enter Key:");
-		//if 
+		String input = key.toUpperCase();
+		if (input.equals("FC")) {
+			float far = RUI.readUserIntInput("Enter Fahrenheit Measurement: ");
+			float toCel = (far - 32) * 5/9;
+			System.out.println(far + " degrees Fahrenheit is " + toCel + " degrees Celsius");
+			
+		} else if (input.equals("CF")) {
+			float cel = RUI.readUserIntInput("Enter Celsius Measurement: ");
+			float toFar = (cel * 9/5) + 32;
+			System.out.println(cel + " degrees Celsius is " + toFar + " degrees Fahrenheit");
+		} else {
+			System.out.println("Invalid Input, Please Try Again");
+		} 
 		
 		
 		
