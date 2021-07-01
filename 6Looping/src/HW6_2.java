eabaf1f15a8280f0689f396599d0b88421fa396a
ZEPHYRUS\karti <kartikeya.vinaykumar@gmail.com>
2021-06-30 21:34:03 -0400

    HW6_2

---
+++ 6Looping/src/HW6_2.java
@@ -0,0 +1,19 @@
+/** 
+ * HW6_2
+ * @author karti
+ *
+ */
+public class HW6_2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int limit = 100;
+		int counter = 1;
+		while (counter <= limit) {
+			System.out.println(counter);
+			counter++;
+		}
+
+	}
+
+}
