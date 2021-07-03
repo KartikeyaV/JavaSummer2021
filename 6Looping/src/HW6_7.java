c280c08f916e46d0117ef5c906af157d357b1c83
ZEPHYRUS\karti <kartikeya.vinaykumar@gmail.com>
2021-07-03 16:27:51 -0400

    HW6_7

--- 6Looping/src/HW6_7.java
+++ 6Looping/src/HW6_7.java
@@ -9,7 +9,7 @@ public class HW6_7 {
 		// TODO Auto-generated method stub
 		for (int i = 0; i<6; i++) {
 			for (int index = 0; index < i; index++) {
-				System.out.print("*");
+				System.out.print("* ");
 			}
 			System.out.println();
 		}
