9468851f0e37970d4c088b509c8a0caf46320c0a
ZEPHYRUS\karti <kartikeya.vinaykumar@gmail.com>
2021-07-03 15:44:16 -0400

    HW6_7

---
+++ 6Looping/src/HW6_7.java
@@ -0,0 +1,15 @@
+/**
+ * HW6_7
+ * @author karti
+ *
+ */
+public class HW6_7 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for (String i ="*"; i.length()<=5; i = i + "*") {
+			System.out.println(i);
+		}
+	}
+
+}
