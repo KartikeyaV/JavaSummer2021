116c47e02d3967779cdf429e866b5a066d4d2b7e
ZEPHYRUS\karti <kartikeya.vinaykumar@gmail.com>
2021-07-03 17:33:27 -0400

    HW6_12

---
+++ 6Looping/src/HW6_12.java
@@ -0,0 +1,34 @@
+/**
+ * HW6_12
+ * @author karti
+ *
+ */
+public class HW6_12 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int fibonacci[] = new int[20];
+		fibonacci[0] = 0;
+		fibonacci[1] = 1;
+		fibonacci[2] = fibonacci[0] + fibonacci[1];
+		fibonacci[3] = fibonacci[1] + fibonacci[2];
+		fibonacci[4] = fibonacci[2] + fibonacci[3];
+		fibonacci[5] = fibonacci[3] + fibonacci[4];
+		fibonacci[6] = fibonacci[4] + fibonacci[5];
+		fibonacci[7] = fibonacci[5] + fibonacci[6];
+		fibonacci[8] = fibonacci[6] + fibonacci[7];
+		fibonacci[9] = fibonacci[7] + fibonacci[8];
+		fibonacci[10] = fibonacci[8] + fibonacci[9];
+		fibonacci[11] = fibonacci[9] + fibonacci[10];
+		fibonacci[12] = fibonacci[10] + fibonacci[11];
+		fibonacci[13] = fibonacci[11] + fibonacci[12];
+		fibonacci[14] = fibonacci[12] + fibonacci[13];
+		fibonacci[15] = fibonacci[13] + fibonacci[14];
+		fibonacci[16] = fibonacci[14] + fibonacci[15];
+		fibonacci[17] = fibonacci[15] + fibonacci[16];
+		fibonacci[18] = fibonacci[16] + fibonacci[17];
+		fibonacci[19] = fibonacci[17] + fibonacci[18];
+		
+	}
+
+}
