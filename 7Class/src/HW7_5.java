c5d2beb7738b4fac90ffb24c1589c452bf8e425f
ZEPHYRUS\karti <kartikeya.vinaykumar@gmail.com>
2021-07-05 23:17:22 -0400

    Unit 7

--- 7Class/src/HW7_5.java
+++ 7Class/src/HW7_5.java
@@ -7,7 +7,19 @@ public class HW7_5 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		String sentence = "The quick brown fox jumps over the lazy dog";
+		int numOfWords = 0;
+		char[] list = sentence.toCharArray();
+		for (int i = 0; i < list.length; i++) {
+			if (Character.isWhitespace(list[i])) {
+				numOfWords++;
+			}
+		}
+		if (list[list.length -1] != ' ') {
+			numOfWords++;
+		}
+		System.out.println(numOfWords);
+		
 	}
 
 }
